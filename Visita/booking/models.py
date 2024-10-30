8a8d0eac2028deaeead2fd3b9d65558bbf2c7ea9
bean_sint <123951763+beansint@users.noreply.github.com>
2024-10-30 22:17:26 +0800

    Entity/model for bookings, lacking the foreign key for customer and rooms

--- Visita/booking/models.py
+++ Visita/booking/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Booking(models.Model):
+    #foreignkey for customer
+    #foreignkey for rooms
+    
+    bookingStatus = models.BooleanField(default=False)
+    bookingStatus = models.CharField(max_length=255)
+    numberOfGuests = models.IntegerField(default=0)
+    
+    checkInDate = models.DateTimeField(null=True, blank=True)
+    checkOutDate = models.DateTimeField(null=True, blank=True)
+
+    createdAt = models.DateTimeField(auto_now_add=True)
+    updatedAt = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return f"Booking {self.id} for Room #roomId - #userID"
