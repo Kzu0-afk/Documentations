48494c0658d126cb8039988f3c61c5ee216d72cb
Judzzxz <judiel.oppura19@gmail.com>
2024-11-17 20:56:11 +0800

    added UI for hotel_list and hotel_form

--- Visita/hotel/views.py
+++ Visita/hotel/views.py
@@ -2,15 +2,22 @@ from django.http import HttpResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .models import Hotel
-from .forms import HotelForm  # Create a form for Hotel in forms.py
-from django.shortcuts import render
+from .forms import HotelForm  # Ensure this form is created in forms.py
 
+# Landing page view
 def landing_page(request):
     return render(request, 'hotel/landing_page.html')
 
+# Functional view for hotel list
+def hotel_list(request):
+    hotels = Hotel.objects.all()
+    return render(request, 'hotel/hotel_list.html', {'hotels': hotels})
+
+# Simple hotel home placeholder view
 def hotel_home(request):
     return HttpResponse("Hotel home page")
 
+# Class-based views (CBVs) for Hotel operations
 class HotelListView(View):
     def get(self, request):
         hotels = Hotel.objects.all()
@@ -25,13 +32,13 @@ class HotelCreateView(View):
         form = HotelForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('hotel_list')
-        return render(request, 'hotel/hotel_form.html', {'form': form})
+            return redirect('hotel_list')  # Redirect to the hotel list after saving
+        return render(request, 'hotel/hotel_form.html', {'form': form})  # Return form with errors
 
 class HotelUpdateView(View):
     def get(self, request, pk):
-        hotel = get_object_or_404(Hotel, pk=pk)
-        form = HotelForm(instance=hotel)
+        hotel = get_object_or_404(Hotel, pk=pk)  # Fetch the hotel object
+        form = HotelForm(instance=hotel)  # Pre-fill the form with existing hotel data
         return render(request, 'hotel/hotel_form.html', {'form': form})
 
     def post(self, request, pk):
@@ -39,11 +46,11 @@ class HotelUpdateView(View):
         form = HotelForm(request.POST, instance=hotel)
         if form.is_valid():
             form.save()
-            return redirect('hotel_list')
+            return redirect('hotel_list')  # Redirect after successful update
         return render(request, 'hotel/hotel_form.html', {'form': form})
 
 class HotelDeleteView(View):
     def get(self, request, pk):
-        hotel = get_object_or_404(Hotel, pk=pk)
-        hotel.delete()
-        return redirect('hotel_list')
+        hotel = get_object_or_404(Hotel, pk=pk)  # Fetch the hotel object
+        hotel.delete()  # Delete the object
+        return redirect('hotel_list')  # Redirect to the list view
