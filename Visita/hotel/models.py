d409c16feddebfe85127a278130966f28f79ba75
Judzzxz <judiel.oppura19@gmail.com>
2024-11-25 00:38:14 +0800

    header and customer landing goods

--- Visita/hotel/models.py
+++ Visita/hotel/models.py
@@ -4,7 +4,7 @@ from admin_entity.models import AdminEntity
 class Hotel(models.Model):
     hotelID = models.AutoField(primary_key=True)
     hotelName = models.CharField(max_length=255)
-    admin = models.ForeignKey(AdminEntity, on_delete=models.CASCADE) #null=True #temporarily fill the admin rows as null b4 having the main admin
+    admin = models.ForeignKey(AdminEntity, on_delete=models.CASCADE, null=True) #null=True #temporarily fill the admin rows as null b4 having the main admin
 
     def __str__(self):
         return self.hotelName
