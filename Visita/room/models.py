0347fdb65459ea56d6af09b21b716cbed7f6fb37
bean_sint <123951763+beansint@users.noreply.github.com>
2024-11-26 04:55:00 +0800

    Revert "Added description and rating for both Hotel and Room models"
    This reverts commit 9ae8dbe831bf15e216d8a37d25afc0e986ed49fb.

--- Visita/room/models.py
+++ Visita/room/models.py
@@ -7,8 +7,6 @@ class Room(models.Model):
     roomType = models.CharField(max_length=50)
     roomPrice = models.DecimalField(max_digits=10, decimal_places=2)
     isAvailable = models.BooleanField(default=True)
-    roomRating = models.IntegerField(max_length=5, null=True, blank=True)
-    roomDescription = models.TextField(null=True, blank=True)
     hotel = models.ForeignKey(Hotel, on_delete=models.CASCADE, related_name='rooms')
 
     def __str__(self):
