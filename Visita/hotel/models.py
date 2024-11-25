9ae8dbe831bf15e216d8a37d25afc0e986ed49fb
bean_sint <123951763+beansint@users.noreply.github.com>
2024-11-26 04:52:48 +0800

    Added description and rating for both Hotel and Room models

--- Visita/hotel/models.py
+++ Visita/hotel/models.py
@@ -4,7 +4,9 @@ from admin_entity.models import AdminEntity
 class Hotel(models.Model):
     hotelID = models.AutoField(primary_key=True)
     hotelName = models.CharField(max_length=255)
-    admin = models.ForeignKey(AdminEntity, on_delete=models.CASCADE, null=True) #null=True #temporarily fill the admin rows as null b4 having the main admin
+    hotelDescription = models.TextField(null=True, blank=True)
+    hotelRating = models.IntegerField(max_length=5, null=True, blank=True)
+    admin = models.ForeignKey(AdminEntity, on_delete=models.CASCADE, null=True)
 
     def __str__(self):
         return self.hotelName
