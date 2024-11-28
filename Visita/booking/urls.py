e4f72ae314ff33c69a1b8c7842876716826b7baa
bean_sint <123951763+beansint@users.noreply.github.com>
2024-11-28 21:55:48 +0800

    booking/ blank redirects to booking_list

--- Visita/booking/urls.py
+++ Visita/booking/urls.py
@@ -4,8 +4,7 @@ from . import views
 app_name = 'booking'
 
 urlpatterns = [
-    path('', views.booking_home, name='booking_home'),
-    path('list/', views.booking_list, name='booking_list'),
+    path('', views.booking_list, name='booking_list'),
     path('create/', views.booking_create, name='booking_create'),
     path('delete/<int:pk>/', views.booking_delete, name='booking_delete'),
     path('rooms/<int:hotel_id>/', views.rooms_for_hotel, name='rooms_for_hotel'),  # Add this route
