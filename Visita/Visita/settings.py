ce2a2032a534d341711d3226a6d683054dc7ba8e
bean_sint <123951763+beansint@users.noreply.github.com>
2024-12-02 02:03:46 +0800

    skibidi settings fix

--- Visita/Visita/settings.py
+++ Visita/Visita/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-!!cbyuop^%%tr*gi0w&*^=!)c*_^otgx-!!!!byukx#8fnxkuj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
